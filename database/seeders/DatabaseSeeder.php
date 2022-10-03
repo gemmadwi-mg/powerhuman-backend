0dd57ffc6191cbda8854eef600689fa1cc8ab832
GDwiP <gemma.19051@mhs.unesa.ac.id.com>
2022-10-03 12:01:47 +0700

    make seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,12 @@
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Company;
+use App\Models\Employee;
+use App\Models\Responsibility;
+use App\Models\Role;
+use App\Models\Team;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,11 +19,17 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        // Company::factory(10)->create();
+        // Team::factory(5)->create();
+        // Role::factory(50)->create();
+        // Responsibility::factory(200)->create();
+        // Employee::factory(1000)->create();
     }
 }
