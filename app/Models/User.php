ae31b5a81d50ff162bc68e43d54a531bfa4c1ae9
GDwiP <gemma.19051@mhs.unesa.ac.id.com>
2022-10-03 12:12:39 +0700

    install jetstream

--- app/Models/User.php
+++ app/Models/User.php
@@ -58,9 +58,4 @@ class User extends Authenticatable
     protected $appends = [
         'profile_photo_url',
     ];
-
-    public function companies()
-    {
-        return $this->belongsToMany(Company::class);
-    }
 }
